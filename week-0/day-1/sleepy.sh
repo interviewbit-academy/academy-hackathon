197264e6851f69746c8ff281fc3f2428d5187256
rohit-kumar6 <rohit.kumar41996@gmail.com>
2019-07-09 12:36:39 +0530

    Modified according to Ubuntu command

--- week-0/day-1/sleepy.sh
+++ week-0/day-1/sleepy.sh
@@ -1,4 +1,4 @@
-open -a "Google Chrome" https://www.youtube.com/watch?v=nkqVm5aiC28&list=RDnkqVm5aiC28
+firefox https://www.youtube.com/watch?v=nkqVm5aiC28&list=RDnkqVm5aiC28
 sleep 15
 echo "I will now shut down"
 # command to shut down laptop
