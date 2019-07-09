0028a812e078784511db6417a996c522b5ecd1f5
Satyajit Das <satyajitd1998@gmail.com>
2019-07-09 22:14:08 +0530

    IB-Academy Week-0 Day-1 Assignment

--- week-0/day-1/wallpaperDownloader.sh
+++ week-0/day-1/wallpaperDownloader.sh
@@ -4,10 +4,10 @@ wget -q -O source.txt https://wallpapercave.com/dark-knight-hd-wallpaper
 grep -oh 'wp/.*.jpg' source.txt >> url.txt
 
 # keep one copy of duplicate lines 
-sort url.txt | uniq >> url.txt
+sort url.txt | uniq >> uniqurl.txt
 
 while IFS= read -r line; 
 do
     wget https://wallpapercave.com/$line
-done < url.txt
+done < uniqurl.txt
   
