d67185bbf51fa8134c79a85d9c725712076aeb99
Abhinav Jha <31755654+Abhinavjha07@users.noreply.github.com>
2019-07-09 14:05:25 +0530

    download Dark Knight wallpapers

--- week-0/day-1/download_image.sh
+++ week-0/day-1/download_image.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-rm -r images
 wget -k https://wallpapercave.com/dark-knight-hd-wallpaper
 
 cat dark-knight-hd-wallpaper | grep img | grep -Po 'src="\K.*?(?=")' | sed 's/\?.*//' | grep -i '/wp/' > links.txt
 
 wget -i links.txt
-mkdir images
+mkdir dark_knight_images
 rm -r links.txt
 rm -r dark-knight-hd-wallpaper
-mv *.jpg images/
+mv *.jpg dark_knight_images/
