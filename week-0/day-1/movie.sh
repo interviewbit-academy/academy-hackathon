ad3e8034dd13b3cccff3354e51e42f3aa7b07eeb
nkg007 <nikhilgupta360174@gmail.com>
2019-07-09 20:55:26 +0530

    Homework day 1

---
+++ week-0/day-1/movie.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+wget -r -P /home/nikhil/Desktop https://wallpapercave.com/the-dark-knight-wallpaper-hd && mv /home/nikhil/Desktop/wallpapercave.com/wp home/nikhil/Desktop/Interviewbit//academy-hackathon/week-0/day-1/
+exit
