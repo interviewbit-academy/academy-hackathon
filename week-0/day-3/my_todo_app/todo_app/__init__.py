e8a791c09f76eb9fb92a56989e4581566fda4c6a
Jasmeet1709 <jsmtbagga45@gmail.com>
2019-07-13 12:49:35 +0530

    crud operations with db

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -3,6 +3,8 @@ import os
 from flask import Flask
 from flask import request
 
+from flask import render_template
+
 
 def create_app(test_config=None):
     # create and configure the app
@@ -17,8 +19,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-
-    def get_todos_by_name(name):
+    
+    def select_todos(name):
         database = db.get_db()
         cursor = database.execute('SELECT tasks FROM todo WHERE name=?',(name,)).fetchall()
         if cursor == None:
@@ -29,9 +31,43 @@ def create_app(test_config=None):
             todos.append(val['tasks'])
         
         return todos
+
+    def insert_todos(name, todo):
+        database = db.get_db()
+        database.execute('INSERT INTO todo (name, tasks) VALUES (?,?)',(name, todo))
+        database.commit()
+        return
+
+    def delete_todo_from_db(name, todo):
+        database = db.get_db()
+        database.execute('DELETE FROM todo WHERE name=? AND tasks=?',(name, todo))
+        database.commit()
+        return
+
+    def update_todo_in_db(name, todo, updatedTodo):
+        database = db.get_db()
+        database.execute('UPDATE todo SET tasks=? WHERE name=? AND tasks=?',(updatedTodo, name, todo))
+        database.commit()
+        return
+    
+    def add_todo_by_name(name,todo):
+        insert_todos(name, todo)
+        return
+
+    def delete_todos_by_name(name, todo):
+        delete_todo_from_db(name, todo)
+        return
+
+    def update_todos_by_name(name, todo, updatedTodo):
+        update_todo_in_db(name, todo, updatedTodo)
+        return
+
+    def get_todos_by_name(name):
+        try:
+            return select_todos(name)
+        except:
+            return None
         
-
-
     # http://127.0.0.1:5000/todos?name=duster
     @app.route('/todos')
     def todos():
@@ -43,10 +79,40 @@ def create_app(test_config=None):
         print('---------')
 
         person_todo_list = get_todos_by_name(name)
+        if person_todo_list == None:
+            return render_template('404.html'), 404
+        else:
+            return render_template('todo_view.html',todos=person_todo_list[:num])        
+    
+    @app.route('/add_todos')
+    def add_todos():
+        name = request.args.get('name')
+        todo = request.args.get('todo')
+        print('------')
+        print(name, todo)
+        print('------')
+        add_todo_by_name(name,todo)
+        return 'Added successfully!!'
 
-        from . import view
-        return view.todo_view(person_todo_list[:num])
+    @app.route('/delete_todos')
+    def delete_todos():
+        name = request.args.get('name')
+        todo = request.args.get('todo')
+        print('------')
+        print(name, todo)
+        print('------')
+        delete_todos_by_name(name, todo)
+        return 'deleted successfully'
 
+    @app.route('/update_todos')
+    def update_todos():
+        name = request.args.get('name')
+        todo = request.args.get('todo')
+        updatedTodo = request.args.get('update')
+        print('------')
+        print(name, todo, updatedTodo)
+        print('------')
+        update_todos_by_name(name, todo, updatedTodo)
+        return 'updated successfully'
     
     return app
-
--- week-0/day-3/my_todo_app/todo_app/view.py
+++
@@ -1,7 +0,0 @@
-def todo_view(todos):
-    the_view = 'List of my todos:' + '<br/>'
-    for todo in todos:
-        the_view += ( todo + '<br/>' )
-
-    the_view += '---- LIST ENDS HERE ---'
-    return the_view
