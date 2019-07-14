3eaf92ce8f6d803d876e71e67fcef6afa36ea3d7
akash <goyalakash1997@gmail.com>
2019-07-14 17:42:34 +0530

    Added fake db and created new view page

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -2,7 +2,12 @@ import os
 
 from flask import Flask
 from flask import request 
+from flask import render_template
 
+todo_store ={}
+todo_store['akash']= ['cricket','music','sleeping']
+todo_store['karthik']= ['crying','playing','eating']
+todo_store['lakshaya']= ['playing','laughing','eating']
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -12,24 +17,36 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    def todo_view(todos):
-        the_view='List of my todos:'+ '<br/>'
-        for todo in todos:
-            the_view +=(todo + '<br/>')
-        the_view +='----LIST ENDS HERE----'
-        return the_view
+    def select_todos(name):
+        global todo_store
+        return todo_store[name]
+    def insert_todos(name,todo):
+        global todo_store
+        current_todos = todo_store[name]
+        current_todos.append(todo)
+        todo_store[name] = current_todos
+        return
+    def add_todo_by_name(name,todo):
+        add_todos(name,todo)
+    def get_todos_by_name(name):
+        try:
+            return select_todos(name)
+        except:
+            return None
+    @app.route('/add_todos')
+    def add_todos():
+        name = request.args.get('name')
+        todo = request.args.get('todo')
+        insert_todos(name,todo)
+        return "added successfully"
     @app.route('/todos')
     def todos():
         name = request.args.get('name')
-        if name=='akash':
-            my_todos=['cricket','music','sleeping']
-        elif name=='karthik':
-            my_todos=['crying','playing','eating']
-        elif name=='lakshaya':
-            my_todos=['playing','laughing','eating']
+        my_todos=get_todos_by_name(name)
+        if my_todos==None:
+            return render_template('404.html'),404
         else:
-            my_todos=[]
-        return todo_view(my_todos)
+            return render_template('todo_view.html',todos=my_todos)
 
     return app
 
