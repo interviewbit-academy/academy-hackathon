e927632f0f8447e4b4b0928625440d7d4d0347d7
aayush <aayush.nirwan97@gmail.com>
2019-07-14 22:49:39 +0530

    DoneTodo

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -1,48 +1,108 @@
-import os
+from flask import Flask,render_template,request
+from flask_mysqldb import MySQL
+import yaml
 
-from flask import Flask
-from flask import request
+app = Flask(__name__)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+#Config db
+db  = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+# app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = db['mysql_db']
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+mysql = MySQL(app)
 
-    # a simple page that list my todos
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == 'POST':
+        #fetch data
+        userDetails = request.form
+        name = userDetails['name']
+        return view(name)
+    
+    return render_template('index.html')
 
-    todolist = {'aayush':['Wake Up','Drink Coffee','Sleep'],'shivang':['Wake Up','Drink Coffee','Read Non-fiction Novel']}
-
-    @app.route('/todos')
-    def todos():
+@app.route('/addtodo',methods=['GET','POST'])
+def addtodo():
+    if request.method == 'POST':
+        det = request.form
         name = request.args.get('name')
-        print(name)
-        if name in todolist:
-        	return todo_view(todolist[name])
-        else:
-                return todo_view([])
+        li = det['todo']
 
-    def todo_view(todlist):
-        the_view = '<h1> List of my todos: </h1>' + '<br/>'
-        for todo in todlist:
-                 the_view+= (todo + '<br/>')
-        the_view+='----- LIST ENDS HERE -----'
-        return the_view
-	
-    @app.route('/shivang')
-    def shivang():
-        mytodo = ['Wake Up','Drink Coffee','Read Non-fiction Novel']
-        return todo_view(mytodo)
-	
-    @app.route('/aayush')
-    def aayush():
-        mytodo = ['Wake Up','Drink Coffee','Sleep']
-        return todo_view(mytodo)
+        cur = mysql.connection.cursor()
+        todolist = cur.execute("SELECT list FROM todoapp where name = '"+name+"'")
+        if todolist>0:
+            list = cur.fetchall()
+            a = list[0][0].split(',')
+        cur.close()
+
+        cur = mysql.connection.cursor()
+        li = list[0][0]+','+li
+        print ("----" + li + "----")
+        cur.execute("UPDATE todoapp SET list = '"+li+"' WHERE name = '"+name+"'")
+        mysql.connection.commit()
+        cur.close()
+
+        print ("Todo Added")
+        return view(name)
+
+@app.route('/view')
+def view(name):
+    cur = mysql.connection.cursor()
+    todolist = cur.execute("SELECT list FROM todoapp where name = '"+name+"'")
+    if todolist>0:
+        list = cur.fetchall()
+        a = list[0][0].split(',')
+        return render_template('view.html',user=name,list=a)
+    else:
+        return ("NO DATA FOUND")
+
+@app.route('/delete')
+def delete():
+    i = request.args.get('index')
+    i = int(i)
+    name = request.args.get('name')
+
+    cur = mysql.connection.cursor()
+    todolist = cur.execute("SELECT list FROM todoapp where name = '"+name+"'")
+    if todolist>0:
+        list = cur.fetchall()
+        a = list[0][0].split(',')
+    cur.close()
+    del(a[i])
+    li=','.join(a)
+    cur = mysql.connection.cursor()
+    cur.execute("UPDATE todoapp SET list = '"+li+"' WHERE name = '"+name+"'")
+    mysql.connection.commit()
+    cur.close()
+    return view(name)
+
+@app.route('/update',methods=['GET','POST'])
+def update():
+    if request.method == 'POST':
+        i = request.args.get('index')
+        i = int(i)
+        name = request.args.get('name')
+        det = request.form
+        updated = det['upded']
+        print (updated)
+        cur = mysql.connection.cursor()
+        todolist = cur.execute("SELECT list FROM todoapp where name = '"+name+"'")
+        if todolist>0:
+            list = cur.fetchall()
+            a = list[0][0].split(',')
+        cur.close()
+
+        a[i]=updated
+        li=','.join(a)
+        cur = mysql.connection.cursor()
+        cur.execute("UPDATE todoapp SET list = '"+li+"' WHERE name = '"+name+"'")
+        mysql.connection.commit()
+        cur.close()
+        return view(name)
 
 
-    return app
 
+if __name__ == '__main__':
+    app.run(debug=True)
