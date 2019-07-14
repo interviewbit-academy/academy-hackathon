425190247de66ab06e654fff4169f7311285193b
Khan Aamir <aamirkhan.khan405@gmail.com>
2019-07-14 09:43:45 +0530

    backdownloader Script

---
+++ week-0/day-1/background_downloader.sh
@@ -0,0 +1,5 @@
+echo Lets Start the Fun...
+echo ###
+wget -nd -r -l1 --no-parent -A jpeg,jpg https://wallpapercave.com/dark-knight-hd-wallpaper | grep -w "dark|knight|batman|robin|dark_knight"
+echo ###
+echo it's Done
