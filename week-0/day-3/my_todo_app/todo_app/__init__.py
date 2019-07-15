51675df2f36ac78937c3a10f26465ed7bfdac5e9
MnmohanSinghThakur <mstnovember@gmail.com>
2019-07-15 19:38:40 +0530

    Fake Database on RAM

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -2,7 +2,11 @@ import os
 
 from flask import Flask
 from flask import request
+from flask import render_template
 
+todo_store={'raj':['Go for a run','Listen to Rock music'],
+			'manmohan':['Study','Brush'],
+			'shivang':['Read Book','Play Fifa']}
 def create_app(test_config=None):
     # create and configure the app
 	app = Flask(__name__, instance_relative_config=True)
@@ -13,24 +17,44 @@ def create_app(test_config=None):
 	except OSError:
 		pass
 	
-	def todo_view(todos):
-		the_view=''
-		for todo in todos:
-			the_view+=todo+'<br>'
-		return the_view
+	def select_todos(name):
+		global todos_store
+		try:
+			return todo_store[name]
+		except:
+			return None
 	
 	def get_todo_name(name):
-		dic={'raj':['Go for a run','Listen to Rock music'],
-			'manmohan':['Study','Brush'],
-			'shivang':['Read Book','Play Fifa']}
-		try:
-			return dic[name]
-		except:
-			return ''
+		return select_todos(name)
+
+	def insert_todo(name,todo):
+		global todo_store
+		current_todos=todo_store[name]
+		current_todos.append(todo)
+		todo_store[name]=current_todos
+
+	def add_todo_by_name(name,todo):
+		insert_todo(name,todo)
+
 	@app.route('/todos')
 	def todos():
 		name=request.args.get('name')
 		print('-----\n'+name+'\n-----')
-		return todo_view(get_todo_name(name))
+		todos_acc_name=get_todo_name(name)
+		if todos_acc_name==None:
+			return render_template('404.html'),404
+		else:
+			return render_template('todo_view.html',todos=todos_acc_name)
 
+	@app.route('/add_todos')
+	def add_todos():
+		name=request.args.get('name')
+		todo=request.args.get('todo')
+		print('------\n',request,name+'\n','------')
+		add_todo_by_name(name,todo)
+		return 'Added Successfully'
+
+	@app.route('/delete_todos')
+	def delete_todos():
+		return 'Deleted Successfully'
 	return app
