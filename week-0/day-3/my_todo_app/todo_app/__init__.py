43f6d8cad0fb50360f2b2cc75b07d4bffcc2bc3a
Saurabh Verma <saurabhvipin55@gmail.com>
2019-07-16 03:50:24 +0530

    saurabh

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -12,25 +12,28 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
+    def todo_view(my_todos):
+        the_view='List of ToDos:'+'<br/>'
+        for todo in my_todos:
+            the_view+=(todo+'<br/>')
+        the_view+='-----LIST ENDS HERE----'
+        return the_view
+        # return (
+        #     'List of ToDos:'+'<br/>'+
+        #     'Wake Up' + '<br/>' +
+        #     'Drink Coffee' + '<br/>' +
+        #     'Read Non-fiction Novel' + '<br/>'+
+        #     '-----LIST ENDS HERE----'
+        # )
     # a simple page that list my todos
     @app.route('/saurabh')
     def saurabh():
-        return (
-            'List of ToDos:'+'<br/>'+
-            'Wake Up' + '<br/>' +
-            'Lunch' + '<br/>' +
-            'Time Pass' + '<br/>'+
-             'Sleep' + '<br/>'
-        )
+        todo=['wake Up','Lunch','Time Pass','Sleep']
+        return todo_view(todo)
 
     @app.route('/shivang')
     def shivang():
-        return (
-            'List of ToDos:'+'<br/>'+
-            'Wake Up' + '<br/>' +
-            'Drink Coffee' + '<br/>' +
-            'Read Non-fiction Novel' + '<br/>'
-        )
+        todo=['Wake Up','Drink Coffee','Study','Sleep']
+        return todo_view(todo)
     return app
 
