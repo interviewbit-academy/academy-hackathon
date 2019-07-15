8aee01f3eba7d856fffd168a3d3831fda7026df5
Prashant Agrawal <a.prashant2020@gmail.com>
2019-07-15 22:47:50 +0530

    new changes

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -2,7 +2,12 @@ import os
 
 from flask import Flask
 from flask import request
-
+from flask import render_template
+todo_store = {}
+todo_store['rahul']=['Go for music','learn swimming','yes']
+todo_store['prashant']=['have to study','have to drink tea','yes']
+todo_store['depo']=['have to study','have to drink coffee','yes']
+todo_store['sanjay']=['have to study economics','have to drink coffee','yes']
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -18,24 +23,58 @@ def create_app(test_config=None):
             the_view+=todo+'<br/>'
         the_view+='----The end of List-----'
         return the_view
+    def select_todos(name):
+        global todo_store
+        return todo_store[name]
     def get_todos_byname(name):
+        try:
+            return select_todos(name)
+        except:
+            return None
+    def insert_todos(name,todo):
+        global todo_store
+        current_todos=todo_store[name]
+        current_todos.append(todo)
+        todo_store[name]=current_todos
+        return
+    def add_todo_byname(name,todo):
+        insert_todos(name,todo)
+        #call DB function
+        return
+    '''def get_todos_byname(name):
         if name=='rahul':
-            return['Go for music','learn swimming']
+            return
         elif name=='shivang':
-            return['speaking French','Learning to cook']
+            return['speaking French','Learning to cook','yes']
         elif name=='prashant':
-            return['have to study','have to drink tea']
+            return['have to study','have to drink tea','yes']
         else:
-            return[]
+            return[]'''
     @app.route('/todos')
     def todos():
         name=request.args.get('name')
+        num=(int)(request.args.get('num'))
         print('---------------')
         print(name)
         print('---------------') 
         my_todos=[]
-        todo_list=get_todos_byname(name)
-        return todo_view(todo_list)
-    # a simple page that list my todos
+        full_todo_list=get_todos_byname(name)
+        if(full_todo_list==None):
+            return render_template ('404.html'),404
+        else:
+            half_todo_list=full_todo_list[0:num]
+        #return todo_view(half_todo_list)
+            return render_template ('todo_view.html', todos=half_todo_list)
+    
+            # a simple page that list my todos
+    @app.route('/add_todos')
+    def add_todos():    #this todos are not persistent as it will work as static data
+        name=request.args.get('name')
+        todo=request.args.get('todo')
+        add_todo_byname(name,todo)
+        print('-----------')
+        print(name,todo)
+        print('-----------')
+        return 'Added Successfully'
     return app
 
