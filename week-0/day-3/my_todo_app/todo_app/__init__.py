e2bd4cc8f2b500db67b8aa47a47c5ff95b7e52c2
manjeet-katiyar <manjeetkatiyar111@gmail.com>
2019-07-18 00:26:52 +0530

    AJAX addedd.

--- week-0/day-3/my_todo_app/todo_app/__init__.py
+++ week-0/day-3/my_todo_app/todo_app/__init__.py
@@ -1,40 +1,30 @@
 import os
-from flask import Flask,request,current_app, render_template,redirect,url_for
-from flaskext.mysql import MySQL
 
+from flask import Flask, request, render_template, redirect, url_for
+
+from . import db
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
 
-    #Getting cursor and conn object from db.py
-    from . import db
-    cursor,conn = db.init_db(app)
-
-    sql_insert_query = """ INSERT INTO `user`
-                          (`username`, `todo`) VALUES (%s,%s)"""
-    sql_get_query = """ SELECT todo from `user`
-                          WHERE username = (%s)"""
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
+    db.initialize_db_app(app)
+
     def select_todos(name):
-        # cursor.execute("SELECT todo from user where username='" + name + "'")
-        cursor.execute(sql_get_query,(name))
-        data = cursor.fetchall()
-        just = []
-        for i in data:
-            just.append(i[0])
-        print("Todos for",name,": ",just)
-        return just
+        db1 = db.Database(app)
+        data = db1.get_todo(name)
+        print("Todos for", name, ": ", data)
+        return data
 
     def insert_todo(name, todo):
-        print("Want to insert",name,todo)
-        cursor.execute(sql_insert_query,(name,todo))
-        conn.commit()
+        db1 = db.Database(app)
+        db1.add_todo(name, todo)
         return
 
     def add_todo_by_name(name, todo):
@@ -45,29 +35,29 @@ def create_app(test_config=None):
     def get_todos_by_name(name):
         return select_todos(name)
 
+    @app.route('/')
+    def index():
+        return render_template('todo_view.html')
+
     @app.route('/todos')
     def todos():
         name = request.args.get('name')
+        print('---------')
+        print(name)
+        print('---------')
 
         person_todo_list = get_todos_by_name(name)
         if person_todo_list == None:
             return render_template('404.html'), 404
         else:
-            return render_template('index.html',todos=person_todo_list,flag = name)
+            return render_template('todo_view.html', todos=person_todo_list)
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-
-    @app.route('/add')
-    def add_t():
-        return render_template('add_todo.html')
-
-    @app.route('/add_todos')
+    @app.route('/add_todos', methods=['POST'])
     def add_todos():
-        name = request.args.get('name')
-        todo = request.args.get('todo')
+        name = request.form.get('name')
+        todo = request.form.get('todo')
         add_todo_by_name(name, todo)
-        return render_template('add_todo.html',name=name,todo=todo)
-        # return redirect(url_for('add_t'))
+        return redirect(url_for('todos', name=name))
+        # return
+
     return app
--- week-0/day-3/my_todo_app/todo_app/db.py
+++ week-0/day-3/my_todo_app/todo_app/db.py
@@ -1,27 +1,48 @@
-from flaskext.mysql import MySQL
-# import os
-# print(os.getcwd())
-f = open("todo_app/schema.sql","r")
-# print(f.read())
+import pymysql
 
-def init_db(app):
-    mysql = MySQL()
-    # MySQL configurations
-    app.config['MYSQL_DATABASE_USER'] = 'manjeet'
-    app.config['MYSQL_DATABASE_PASSWORD'] = 'Manjeet@12345'
-    app.config['MYSQL_DATABASE_DB'] = 'flask_app'
-    app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-    mysql.init_app(app)
-    conn = mysql.connect()
-    cursor =conn.cursor()
-
-    cursor.execute("DROP TABLE IF EXISTS user")
+def initialize_db_app(app):
+    db1 = Database(app)
+    db1.cur.execute("DROP TABLE IF EXISTS user")
+    f = open("todo_app/schema.sql", "r")
     sqlCommands = f.read().split(';')
-
+    db1.cur.execute("DROP TABLE IF EXISTS user")
     for command in sqlCommands:
         try:
-            cursor.execute(command)
-        except :
-            print("Skipped: ",command)
-    conn.commit()
-    return cursor,conn
+            print("Executing: ", command)
+            db1.cur.execute(command)
+        except:
+            print("Skipped: ", command)
+
+    db1.con.commit()
+
+
+class Database:
+    def __init__(self, app):
+        host = "localhost"
+        user = "manjeet"
+        password = "Manjeet@12345"
+        db = "flask_app"
+        self.con = pymysql.connect(host=host, user=user, password=password, db=db,
+                                   cursorclass=pymysql.cursors.DictCursor)
+        self.cur = self.con.cursor()
+
+    def get_todo(self, name):
+        sql_get_query = """ SELECT todo from `user`
+                          WHERE username = (%s)"""
+
+        self.cur.execute(sql_get_query, (name))
+        result = self.cur.fetchall()
+        just = []
+        for item in result:
+            just.append(item['todo'])
+        print("-----------------")
+        print(just)
+        print("-----------------")
+        return just
+
+    def add_todo(self, name, todo):
+        sql_insert_query = """ INSERT INTO `user`
+                          (`username`, `todo`) VALUES (%s,%s)"""
+        print("Want to insert -> ", (name, todo))
+        self.cur.execute(sql_insert_query, (name, todo))
+        self.con.commit()
